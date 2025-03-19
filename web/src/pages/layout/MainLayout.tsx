e03011f7b76a75d0656d1414ce3107dc5b7b194c
chenyanxun000@163.com <chenyanxun000@163.com>
2025-03-19 19:45:11 +0800

    UI first commit

--- web/src/pages/layout/MainLayout.tsx
+++ web/src/pages/layout/MainLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
-import styles from "./layout.module.scss"
+import styles from "./layout.module.scss";
 import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
 function MainLayout() {
   return (
     <div className={styles.layout}>
       <Header></Header>
-      <Outlet />
+      <div className={styles.content}>
+        <Outlet />
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
